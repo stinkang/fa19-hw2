ac2df3323d906f9bed765f42d4181feba5756a51
Austin Kang <austinkang@austins-mbp-5.lan>
2019-10-02 15:41:08 -0700

    whatever message you want

--- hw2.rb
+++ hw2.rb
@@ -1,19 +1,37 @@
 def squared_sum(a, b)
   # Q1 CODE HERE
+	return (a + b)**2
 end
 
 def sort_array_plus_one(a)
   # Q2 CODE HERE
+	b = a.sort
+	i = 0
+	while i < b.length
+		b[i] = b[i] + 1
+		i = i + 1
+	end
+	return b
 end
 
 def combine_name(first_name, last_name)
   # Q3 CODE HERE
+	return first_name + " " + last_name
 end
 
 def blockin_time(a)
   # DO NOT EDIT THIS CODE BELOW
   require './foobar'
   Foobar.baz a
+	b = []
+	i = 0
+	while i < a.length
+		if !(a[i].to_i + 2 >= 10) and !((a[i].to_i + 2) % 2 == 1) and !(b.include?(a[i].to_i + 2))
+			b.concat [a[i].to_i + 2]
+		end
+		i = i + 1
+	end
+	b.sum
 end
 
 def scrabble(word)
